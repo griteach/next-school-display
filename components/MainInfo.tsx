1340ff945bd8bdc70ac3d6233e180276ce76e7b1
griteach <griteach22@gmail.com>
2023-02-19 00:36:54 +0900

    grid update

--- components/MainInfo.tsx
+++ components/MainInfo.tsx
@@ -4,12 +4,11 @@ import SideBar from "./SideBar";
 export default function MainInfo({ response }: IGetDustResult) {
   return (
     <>
-      <div className="grid w-full h-full px-8 py-8 grid-cols-[1fr_3fr] grid-rows-1 bg-gradient-to-r from-mojito-l to-mojito-r">
-        {/* <div className="columns-2 hover:columns-3"> */}
-        <div className="rounded-bl-2xl rounded-tl-2xl ">
+      <div className="grid w-full h-full px-8 py-8 grid-cols-[1fr_3fr] grid-rows-1 bg-gradient-to-r from-mojito-l to-mojito-r gap-1">
+        <div className=" rounded-2xl">
           <SideBar title={`서원초등학교`} />
         </div>
-        <div className="bg-[#F3E8F5] rounded-br-2xl rounded-tr-2xl"></div>
+        <div className="bg-[#F3E8F5] rounded-2xl"></div>
       </div>
     </>
   );
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,8 +1,10 @@
 import TitleComponent, { ITitleComponentProps } from "./TitleComponent";
 
 import LottieWeatherPartlyShower from "../pages/lottie/weather/weather-partly-shower.json";
-import LottieBalloons from "../pages/lottie/etc/balloons-with-string.json";
+
 import Lottie from "react-lottie";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Smiley, Heart, Cloud, CloudFog } from "phosphor-react";
 
 export default function SideBar({ title }: ITitleComponentProps) {
   const weatherOptions = {
@@ -15,22 +17,13 @@ export default function SideBar({ title }: ITitleComponentProps) {
     },
   };
 
-  const balloonsOptions = {
-    loot: true,
-    autoplay: true,
-    animationData: LottieBalloons,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-
   return (
-    <div className="h-full w-full grid grid-cols-1 gap-1 grid-rows-[1fr_3fr_3fr_2fr] ">
-      <div className="bg-[#F3E8F5] rounded-tl-2xl">
+    <div className="bg-[#F0E4F2]  h-full w-full grid grid-cols-1  grid-rows-[1fr_3fr_3fr_2fr] rounded-2xl">
+      <div className="rounded-tl-2xl rounded-tr-2xl">
         <TitleComponent title={title} />
       </div>
-      <div className="bg-[#F3E9F5] flex flex-col justify-evenly items-center px-4 py-12 ">
-        <div className="w-full h-full bg-gradient-to-r from-weather-l to-weather-r rounded-3xl flex">
+      <div className="flex flex-col justify-center items-center px-4 py-6">
+        <div className="w-full h-full bg-gradient-to-r from-weather-l to-weather-r rounded-3xl flex shadow-xl">
           <div className="w-1/2 h-full flex flex-col justify-center items-center p-4">
             <div className="">
               <Lottie
@@ -46,8 +39,8 @@ export default function SideBar({ title }: ITitleComponentProps) {
           </div>
           <div className="w-1/2 h-full flex flex-col justify-center items-center ">
             <div className="text-white">
-              <span className="text-5xl">21</span>
-              <span className="text-5xl">°</span>
+              <span className="text-6xl">21</span>
+              <span className="text-6xl">°</span>
               <div>
                 <span className="text-[10px] text-white">체감온도 26°</span>
               </div>
@@ -55,8 +48,25 @@ export default function SideBar({ title }: ITitleComponentProps) {
           </div>
         </div>
       </div>
-      <div className="bg-[#F3E8F5] flex flex-col justify-evenly items-start "></div>
-      <div className="bg-[#F3E8F5] rounded-bl-2xl flex flex-col justify-evenly items-start"></div>
+      <div className=" flex flex-col px-4 py-6">
+        <div className="bg-white w-full h-full rounded-3xl shadow-xl flex flex-col">
+          <div className="flex justify-start items-center">
+            <div>
+              <CloudFog size={32} color="#938FF2" />
+            </div>
+            <div>오늘 하늘은?</div>
+          </div>
+          <div className="w-full h-full bg-blue-400 grid grid-cols-3 grid-rows-2 justify-items-center items-center p-2 ">
+            <div className="bg-red-300 ">1</div>
+            <div className="bg-red-300 ">2</div>
+            <div className="bg-red-300 ">3</div>
+            <div className="bg-red-300 ">4</div>
+            <div className="bg-red-300 ">5</div>
+            <div className="bg-red-300 ">6</div>
+          </div>
+        </div>
+      </div>
+      <div className=" flex flex-col rounded-bl-2xl rounded-br-2xl"></div>
     </div>
   );
 }
