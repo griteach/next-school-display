b5186a497e5d23bd11ca4153b754c7d6e7ef6772
griteach <griteach22@gmail.com>
2023-02-22 01:22:36 +0900

    weather fin

--- components/AirQuality.tsx
+++ components/AirQuality.tsx
@@ -1,3 +1,5 @@
+import { GET_WEATHER, IWeatherGql } from "@/modules/apollo";
+import { useQuery } from "@apollo/client";
 import {
   Smiley,
   Wind,
@@ -9,6 +11,28 @@ import {
 } from "phosphor-react";
 
 export default function AirQuality() {
+  const { data: weatherData, loading: weatherDataLoading } =
+    useQuery<IWeatherGql>(GET_WEATHER);
+  const reh = weatherData?.allWeather.find(function (item) {
+    if (item.category === "REH") {
+      return item.obsrValue;
+    }
+  });
+  const wsd = weatherData?.allWeather.find(function (item) {
+    if (item.category === "WSD") {
+      return item.obsrValue;
+    }
+  });
+  const pty = weatherData?.allWeather.find(function (item) {
+    if (item.category === "PTY") {
+      return item.obsrValue;
+    }
+  });
+  const rn1 = weatherData?.allWeather.find(function (item) {
+    if (item.category === "RN1") {
+      return item.obsrValue;
+    }
+  });
   return (
     <>
       <div className="bg-white w-full h-full rounded-3xl shadow-xl flex flex-col ">
@@ -25,8 +49,8 @@ export default function AirQuality() {
                 <Thermometer size={28} color="#938FF2" />
               </div>
               <div className="w-2/3 h-full flex flex-col justify-center items-start text-xs ml-1">
-                <div>체감온도</div>
-                <div>23.8</div>
+                <div>강수량</div>
+                <div>{rn1?.obsrValue}</div>
               </div>
             </div>
             <div className="w-1/3 h-full  flex justify-center items-center text-xs">
@@ -35,7 +59,7 @@ export default function AirQuality() {
               </div>
               <div className="w-2/3 h-full flex flex-col justify-center items-start ml-1">
                 <div>풍속</div>
-                <div>23.8</div>
+                <div>{wsd?.obsrValue}</div>
               </div>
             </div>
             <div className="w-1/3 h-full  flex justify-center items-center text-xs">
@@ -63,8 +87,8 @@ export default function AirQuality() {
                 <Sunglasses size={28} color="#938FF2" />
               </div>
               <div className="w-2/3 h-full flex flex-col justify-center items-start ml-1">
-                <div>자외선</div>
-                <div>23.8</div>
+                <div>습도</div>
+                <div>{reh?.obsrValue}</div>
               </div>
             </div>
             <div className="w-1/3 h-full  flex justify-center items-center text-xs">
@@ -72,8 +96,8 @@ export default function AirQuality() {
                 <CloudFog size={32} color="#938FF2" />
               </div>
               <div className="w-2/3 h-full flex flex-col justify-center items-start ml-1">
-                <div>오존</div>
-                <div>23.8</div>
+                <div>강수</div>
+                <div>{pty?.obsrValue}</div>
               </div>
             </div>
           </div>
