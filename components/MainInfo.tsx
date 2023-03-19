ed022b546258e4c4fccb5ae3e0067cdc70aed2a0
griteach <griteach22@gmail.com>
2023-03-20 00:55:09 +0900

    delete getServerSideProps

--- components/MainInfo.tsx
+++ components/MainInfo.tsx
@@ -1,8 +1,7 @@
-import { IGetDustResult } from "@/modules/typeDefinition";
 import MainBar from "./MainBar";
 
 import SideBar from "./SideBar";
-export default function MainInfo({ response }: IGetDustResult) {
+export default function MainInfo() {
   return (
     <>
       <div className="grid w-full h-full p-8 grid-cols-[1fr_3fr] grid-rows-1 bg-blue-500   gap-3">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import MainInfo from "@/components/MainInfo";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
 import Seo from "./seo";
-export default function Home({
-  result,
-}: InferGetServerSidePropsType<GetServerSideProps>) {
+export default function Home() {
   return (
     <>
       <div className="w-screen h-screen flex">
         {/* Seo component */}
         <Seo title="서원초등학교"></Seo>
-        <MainInfo response={result} />
+        <MainInfo />
       </div>
     </>
   );
@@ -19,24 +17,22 @@ export default function Home({
 //Server SIde Rendering
 //여기에 작성되는 코드는 서버에서 실행되는 것! 서버 사이드이기 때문!
 
-export async function getServerSideProps() {
-  //미세먼지 기본 패쓰
-  const DUST_PATH_BASIC = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc";
-  //측정소별 URL
-  const DUST_URL = "/getCtprvnRltmMesureDnsty";
-  const result = await (
-    await fetch(
-      `${DUST_PATH_BASIC}${DUST_URL}?serviceKey=${
-        process.env.API_KEY
-      }&numOfRows=100&returnType=json&ver=1.3&sidoName=${encodeURIComponent(
-        "강원"
-      )}`
-    )
-  ).json();
-
-  console.log("getServerSideProps에서 실행완료");
-
-  return {
-    props: { result },
-  };
-}
+// export async function getServerSideProps() {
+//   //미세먼지 기본 패쓰
+//   // const DUST_PATH_BASIC = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc";
+//   // //측정소별 URL
+//   // const DUST_URL = "/getCtprvnRltmMesureDnsty";
+//   // const result = await (
+//   //   await fetch(
+//   //     `${DUST_PATH_BASIC}${DUST_URL}?serviceKey=${
+//   //       process.env.API_KEY
+//   //     }&numOfRows=100&returnType=json&ver=1.3&sidoName=${encodeURIComponent(
+//   //       "강원"
+//   //     )}`
+//   //   )
+//   // ).json();
+//   // console.log("getServerSideProps에서 실행완료");
+//   // return {
+//   //   props: { result },
+//   // };
+// }
