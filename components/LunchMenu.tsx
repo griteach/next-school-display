81e09c3901ece80f740f351d27a2a8a3a4089254
griteach <griteach22@gmail.com>
2023-04-10 10:42:35 +0900

    fix meal data

--- components/LunchMenu.tsx
+++ components/LunchMenu.tsx
@@ -6,9 +6,9 @@ interface IMenuProps {
 export default function LunchMenu(menuProps: IMenuProps) {
   return (
     <div className="w-full h-full  flex justify-center items-center">
-      <div className="w-2/3 bg-red-400 rounded-xl">
-        <div className="flex justify-center items-center ">
-          {menuProps.menu}
+      <div className="w-full h-14 mb-2 flex justify-center  items-center  rounded-full">
+        <div className="flex justify-center items-center text-3xl ">
+          {menuProps.menu.slice(0, menuProps.menu.indexOf("("))}
         </div>
       </div>
     </div>
